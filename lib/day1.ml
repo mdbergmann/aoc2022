4193aec6ad79925d1bfe9d6f2df77cb2398849a4
Manfred Bergmann <mb@software-by-mabe.com>
2022-12-01 12:54:49 +0100

    TCR: test OK

--- lib/day1.ml
+++ lib/day1.ml
@@ -17,7 +17,9 @@ let test_input = "1000
 10000"
 
 
-let day_1 input = (List.group (String.split input ~on:'\n') ~break:(fun a _ -> String.(=) a ""))
+let day_1 input = input
+                  |> String.split ~on:'\n'
+                  |> List.group ~break:(fun a _ -> String.(=) a "")
 
 let%test "day 1 - demo test" =
   let result = day_1 test_input
