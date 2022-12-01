f9e9863fba6b8d4e7eaf8c3563a2c5899a697333
Manfred Bergmann <mb@software-by-mabe.com>
2022-12-01 13:12:47 +0100

    TCR: test OK

--- lib/day1.ml
+++ lib/day1.ml
@@ -22,11 +22,16 @@ let inspect x = ExtLib.print x; x
 let day_1 input = input
                   |> String.split ~on:'\n'
                   |> List.group ~break:(fun a _ -> String.(=) a "")
-                  |> List.map ~f:(fun n -> List.filter n ~f:(fun str -> not (String.is_empty str)))
-                  |> List.length
+                  |> List.map ~f:(fun lst -> List.filter lst ~f:(fun str -> not (String.is_empty str)))
+                  |> List.map ~f:(fun lst -> List.map lst ~f:Int.of_string)
+                  |> List.map ~f:(fun lst -> List.fold lst ~init:0 ~f:(+))
+                  |> List.sort ~compare:(fun a b -> if a > b then -1
+                                                    else if a < b then 1
+                                                    else 0)
+                  |> List.hd_exn
 
 let%test "day 1 - demo test" =
   let result = day_1 test_input
   in
   printf "Result: %s\n" (ExtLib.dump result);
-  result = 5
+  result = 24000
