56bf041ad6cde6c2ebdbcb1aa5e467fe75e22cc8
Manfred Bergmann <mb@software-by-mabe.com>
2022-12-01 13:31:10 +0100

    TCR: test OK

--- lib/day1.ml
+++ lib/day1.ml
@@ -19,7 +19,7 @@ let test_input = "1000
 
 let inspect x = ExtLib.print x; x
 
-let day_1 input =
+let day_1 input select_fun =
   let filter_empty_strings lst = List.filter lst ~f:(fun str -> not (String.is_empty str)) in
   let convert_to_int lst = List.map lst ~f:Int.of_string in
   let sum lst = List.fold lst ~init:0 ~f:(+) in
@@ -33,20 +33,25 @@ let day_1 input =
   |> List.map ~f:convert_to_int
   |> List.map ~f:sum
   |> List.sort ~compare:sort_desc
-  |> List.hd_exn
+  |> select_fun
 
 let%test "day 1 - demo test" =
-  let result = day_1 test_input
+  let result = day_1 test_input (fun lst -> List.hd_exn lst)
   in
-  printf "Result: %s\n" (ExtLib.dump result);
+  printf "Result 1: %s\n" (ExtLib.dump result);
   result = 24000
 
 let prep_input =
   In_channel.read_all "/Users/mbergmann/Development/MySources/aoc2022/input/day1_1.txt"
 
 let%test "day 1 - full test" =
-  let result = day_1 prep_input
+  let result = day_1 prep_input (fun lst -> List.hd_exn lst)
   in
-  printf "Result: %s\n" (ExtLib.dump result);
+  printf "Result 1: %s\n" (ExtLib.dump result);
   result = 71300
 
+let%test "day 1-2 - demo test" =
+  let result = day_1 test_input (fun lst -> List.fold (List.take lst 3) ~init:0 ~f:(+))
+  in
+  printf "Result 1-2: %s\n" (ExtLib.dump result);
+  true
