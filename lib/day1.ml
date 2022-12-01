6c2d12e2a88d199c367ed7d9d566d7df197ce72e
Manfred Bergmann <mb@software-by-mabe.com>
2022-12-01 13:00:09 +0100

    TCR: test OK

--- lib/day1.ml
+++ lib/day1.ml
@@ -21,12 +21,12 @@ let inspect x = ExtLib.print x; x
 
 let day_1 input = input
                   |> String.split ~on:'\n'
-                  |> inspect
                   |> List.group ~break:(fun a _ -> String.(=) a "")
                   |> List.map ~f:(fun n -> List.filter n ~f:(fun str -> not (String.is_empty str)))
+                  |> List.length
 
 let%test "day 1 - demo test" =
   let result = day_1 test_input
   in
   printf "Result: %s\n" (ExtLib.dump result);
-  true
+  result = 5
