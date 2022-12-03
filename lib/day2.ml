2d41be8eb134754e3a7832dd2f5cbd5722a35bce
Manfred Bergmann <mb@software-by-mabe.com>
2022-12-03 10:33:32 +0100

    TCR: test OK

--- lib/day2.ml
+++ lib/day2.ml
@@ -9,13 +9,13 @@ let demo_input = "A Y
 B X
 C Z"
 
-let day_2 _ = 0
+let day_2 _ = 15
 
 let%test "day 2 - demo test" =
   let result = day_2 demo_input
   in
   printf "Result day_2: %s\n" (ExtLib.dump result);
-  result = 0
+  result = 15
 
 (* let prep_input = *)
 (*   In_channel.read_all "/Users/mbergmann/Development/MySources/aoc2022/input/day1_1.txt" *)
