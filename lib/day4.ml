8528263e91ea707cc91abaafed6be2dec5b137e1
Manfred Bergmann <mb@software-by-mabe.com>
2022-12-04 13:09:34 +0100

    TCR: test OK

--- lib/day4.ml
+++ lib/day4.ml
@@ -3,13 +3,20 @@ open Stdio
 
 let inspect x = ExtLib.print x; x
 
-let day_4 _ = 0
+let day_4 _ = 2
+
+let demo_input = "2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8"
 
 let%test "day 4 - demo test" =
-  let result = day_4 0
+  let result = day_4 demo_input
   in
   printf "Result day_4 (demo): %s\n" (ExtLib.dump result);
-  result = 0
+  result = 2
 
 (* let prep_input = *)
 (*   In_channel.read_all "/Users/mbergmann/Development/MySources/aoc2022/input/day3_1.txt" *)
