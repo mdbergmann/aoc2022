ade0af81df977ffdf7b68e2e6e861077bc94526f
Manfred Bergmann <mb@software-by-mabe.com>
2022-12-04 12:48:54 +0100

    TCR: test OK

--- lib/day3.ml
+++ lib/day3.ml
@@ -5,16 +5,7 @@ let inspect x = ExtLib.print x; x
 
 exception Error
 
-let day_3 input =
-  let rucksacks = String.split_lines input in
-  let all_compartments = List.map rucksacks ~f:(fun rucksack ->
-                             let rucksack_len = String.length rucksack in
-                             let half_len = rucksack_len / 2 in
-                             (String.sub rucksack ~pos:0 ~len:half_len,
-                              String.sub rucksack ~pos:half_len ~len:half_len)) in
-  let shared_items = List.map all_compartments ~f:(fun (comp1, comp2) ->
-                         String.filter comp1 ~f:(fun item1 ->
-                             String.exists comp2 ~f:(fun item2 -> Char.(=) item1 item2))) in
+let common_day_3 shared_items =
   let priorities = List.map shared_items ~f:(fun items ->
                        List.map (String.to_list items) ~f:(fun item ->
                            let char_int = Char.to_int item in
@@ -26,6 +17,18 @@ let day_3 input =
                       List.fold rucksack_prios ~init:0 ~f:(+)) in
   let sum = List.fold sack_sums ~init:0 ~f:(+) in
   sum
+
+let day_3_1 input =
+  let rucksacks = String.split_lines input in
+  let all_compartments = List.map rucksacks ~f:(fun rucksack ->
+                             let rucksack_len = String.length rucksack in
+                             let half_len = rucksack_len / 2 in
+                             (String.sub rucksack ~pos:0 ~len:half_len,
+                              String.sub rucksack ~pos:half_len ~len:half_len)) in
+  let shared_items = List.map all_compartments ~f:(fun (comp1, comp2) ->
+                         String.filter comp1 ~f:(fun item1 ->
+                             String.exists comp2 ~f:(fun item2 -> Char.(=) item1 item2))) in
+  common_day_3 shared_items
 
 let day_3_2 input =
   let rucksacks = String.split_lines input in
@@ -39,8 +42,8 @@ let day_3_2 input =
                                   (String.exists sack3 ~f:(fun item3 -> Char.(=) item1 item3)))
                          | _ -> raise Error
                        ) in
-  ExtLib.print shared_items;
-  70
+  common_day_3 shared_items
+
 
 let demo_input = "vJrwpWtwJgWrhcsFMMfFFhFp
 jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
@@ -50,7 +53,7 @@ ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw"
 
 let%test "day 3 - demo test" =
-  let result = day_3 demo_input
+  let result = day_3_1 demo_input
   in
   printf "Result day_3 (demo): %s\n" (ExtLib.dump result);
   result = 157
@@ -59,7 +62,7 @@ let prep_input =
   In_channel.read_all "/Users/mbergmann/Development/MySources/aoc2022/input/day3_1.txt"
 
 let%test "day 3 - real test" =
-  let result = day_3 prep_input
+  let result = day_3_1 prep_input
   in
   printf "Result day_3 (real): %s\n" (ExtLib.dump result);
   result = 7568
