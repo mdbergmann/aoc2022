8b25ea896b17be7598fddc519c2c65e387b900f3
Manfred Bergmann <mb@software-by-mabe.com>
2022-12-04 12:32:41 +0100

    TCR: test OK

--- lib/day3.ml
+++ lib/day3.ml
@@ -10,24 +10,19 @@ let day_3 input =
                              let half_len = rucksack_len / 2 in
                              (String.sub rucksack ~pos:0 ~len:half_len,
                               String.sub rucksack ~pos:half_len ~len:half_len)) in
-  ExtLib.print all_compartments;
   let shared_items = List.map all_compartments ~f:(fun (comp1, comp2) ->
                          String.filter comp1 ~f:(fun item1 ->
                              String.exists comp2 ~f:(fun item2 -> Char.(=) item1 item2))) in
-  ExtLib.print shared_items;
   let priorities = List.map shared_items ~f:(fun items ->
                        List.map (String.to_list items) ~f:(fun item ->
                            let char_int = Char.to_int item in
                            if char_int >= 97 then char_int - 96
                            else char_int - 38)) in
-  ExtLib.print priorities;
   let filtered_dups = List.map priorities ~f:(fun prios ->
                           List.dedup_and_sort prios ~compare:(fun _ _ -> 0)) in
-  ExtLib.print filtered_dups;
   let sack_sums = List.map filtered_dups ~f:(fun rucksack_prios ->
                       List.fold rucksack_prios ~init:0 ~f:(+)) in
   let sum = List.fold sack_sums ~init:0 ~f:(+) in
-  ExtLib.print sum;
   sum
 
 let demo_input = "vJrwpWtwJgWrhcsFMMfFFhFp
