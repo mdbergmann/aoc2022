8a0405175745bae2982f6a7bc8f91a50e5627177
Manfred Bergmann <mb@software-by-mabe.com>
2022-12-05 20:21:00 +0100

    TCR: test OK

--- lib/day5.ml
+++ lib/day5.ml
@@ -11,15 +11,19 @@ let make_range s e  =
 
 let day_5 input =
   let lines = String.split_lines input in
-  let (creates_numbers_line_index, creates_numbers_line) = match
+  let (crates_numbers_line_index, crates_numbers_line) = match
       List.findi lines ~f:(fun _ line -> not (String.contains line '[')) with
     | Some (l_i, line) -> (l_i, line)
     | None -> assert false in
-  ExtLib.print (creates_numbers_line_index, creates_numbers_line);
-  let stack_lines = List.sub lines ~pos:0 ~len:creates_numbers_line_index in
-  let move_lines = List.drop lines (creates_numbers_line_index+2) in
+  ExtLib.print (crates_numbers_line_index, crates_numbers_line);
+  let stack_lines = List.sub lines ~pos:0 ~len:crates_numbers_line_index in
+  let move_lines = List.drop lines (crates_numbers_line_index+2) in
   ExtLib.print stack_lines;
-  ExtLib.print move_lines;  
+  ExtLib.print move_lines;
+  let four_cols = ((String.length crates_numbers_line) - 4) / 4 in
+  let col_indices = 2 :: List.map ~f:(fun c -> 2 + (c * 4))
+                           (make_range 1 (four_cols+1)) in
+  ExtLib.print col_indices;
   "CMZ"
 
 let demo_input = "    [D]    
