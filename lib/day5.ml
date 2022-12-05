fc7f1a00796103154978405d3ecea7aa6addb0b2
Manfred Bergmann <mb@software-by-mabe.com>
2022-12-05 18:32:53 +0100

    TCR: test OK

--- lib/day5.ml
+++ lib/day5.ml
@@ -11,6 +11,17 @@ let make_range s e  =
 
 let day_5 = 0
 
+
+let demo_input = "    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2"
+
 let%test "day 5 - demo test" =
   let result = day_5
   in
@@ -18,4 +29,4 @@ let%test "day 5 - demo test" =
   result = 0
 
 (* let prep_input = *)
-(*   In_channel.read_all "/Users/mbergmann/Development/MySources/aoc2022/input/day4_1.txt" *)
+(*   In_channel.read_all "/Users/mbergmann/Development/MySources/aoc2022/input/day5_1.txt" *)
