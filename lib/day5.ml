88eaa790983e928cc170f41f2119aa35d86c6954
Manfred Bergmann <mb@software-by-mabe.com>
2022-12-05 20:55:25 +0100

    TCR: test OK

--- lib/day5.ml
+++ lib/day5.ml
@@ -31,7 +31,7 @@ let day_5 input =
   List.iter stack_lines ~f:(fun stack_line ->
       List.iter col_stacks ~f:(fun (col_index, crate_stack) ->
           let crate_id = Char.to_string (String.get stack_line col_index) in
-          if not (String.equal crate_id "") then Stack.push crate_stack crate_id
+          if not (String.equal crate_id " ") then Stack.push crate_stack crate_id
         )
     );
   ExtLib.print col_stacks;
