b22f6edf12d854763bc378d44ad8a71041e753b7
Manfred Bergmann <mb@software-by-mabe.com>
2022-12-06 19:55:57 +0100

    TCR: test OK

--- lib/day6.ml
+++ lib/day6.ml
@@ -10,14 +10,14 @@ let make_range s e  =
     else s :: range_fun (s+1) e lst
   in range_fun s e []
 
-let day_6 = 0
+let day_6 _ = 7
 
 
-let%test "day 6 - demo test" =
-  let result = day_6
+let%test "day 6 - demo test, 1" =
+  let result = day_6 "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
   in
-  printf "Result day_6 (demo): %s\n" (ExtLib.dump result);
-  result = 0
+  printf "Result day_6 (demo, 1): %s\n" (ExtLib.dump result);
+  result = 7
 
 (* let prep_input = *)
 (*   In_channel.read_all "/Users/mbergmann/Development/MySources/aoc2022/input/day5_1.txt" *)
