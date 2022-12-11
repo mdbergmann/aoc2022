54251b7acbdd5bbc1a5d8cbcd0f8b39f4239e0ba
Manfred Bergmann <mb@software-by-mabe.com>
2022-12-11 11:27:35 +0100

    TCR: test OK

--- lib/day8.ml
+++ lib/day8.ml
@@ -11,13 +11,13 @@ let make_range s e  =
 
 let day_8 input =
   let lines = String.split_lines input in
-  let rows = List.to_array (List.map lines ~f:(fun line ->
-                                Array.map (String.to_array line) ~f:(fun c ->
-                                    (Char.to_int c) - (Char.to_int '0')))) in
+  let rows = List.map lines ~f:(fun line ->
+                 List.map (String.to_list line) ~f:(fun c ->
+                     (Char.to_int c) - (Char.to_int '0'))) in
   let visible_trees_from_left_on_row row =
     let visible_index = ref 0 in
     let visible_elem = ref 0 in
-    Array.iteri row ~f:(fun i elem ->
+    List.iteri row ~f:(fun i elem ->
         if elem > !visible_elem
         then (
           visible_elem := elem;
@@ -29,14 +29,14 @@ let day_8 input =
   let visible_trees_from_right_on_row row =
     let visible_index = ref 0 in
     let visible_elem = ref 0 in
-    Array.iteri (Array.rev row) ~f:(fun i elem ->
+    List.iteri (List.rev row) ~f:(fun i elem ->
         if elem > !visible_elem
         then (
           visible_elem := elem;
           visible_index := i;
       ));
     let adjusted_index =
-      (Array.length row) - (!visible_index + 1) in
+      (List.length row) - (!visible_index + 1) in
     (!visible_elem, adjusted_index)
   in
 
@@ -44,21 +44,22 @@ let day_8 input =
     let (_, from_left_index) = visible_trees_from_left_on_row row in
     let (_, from_right_index) = visible_trees_from_right_on_row row in
 
-    if (from_left_index > 0) && (from_right_index < (Array.length row))
+    if (from_left_index > 0) && (from_right_index < (List.length row))
     then 
       if (from_left_index = from_right_index)
       then 1
       else 2
     else 0 in
 
-  let visible_trees_rows = (Array.fold (Array.map rows
+  let effective_rows = (List.drop_last_exn (List.drop rows 1)) in
+  let visible_trees_rows = (List.fold (List.map effective_rows
                                          ~f:visible_trees_count_hori_inner)
                              ~init:0
                              ~f:(+))
-                           + ((Array.length rows) * 2) in
+                           + ((List.length effective_rows) * 2) in
   
   ExtLib.print visible_trees_rows;
-  assert (visible_trees_rows = 5 + 10);
+  assert (visible_trees_rows = 9);
   21
 
 let demo_input = "30373
